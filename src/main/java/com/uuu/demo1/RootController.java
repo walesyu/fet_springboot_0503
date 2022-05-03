e8f565544fe8f402bfeaf938a5c961dbc3497e9d
Wilson Yu <wilsonyu@fareastone.com.tw>
2022-05-03 13:41:39 +0800

    first commit

---
+++ src/main/java/com/uuu/demo1/RootController.java
@@ -0,0 +1,12 @@
+package com.uuu.demo1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+  @GetMapping("/")
+  public String home(){
+    return "home";
+  }
+}
