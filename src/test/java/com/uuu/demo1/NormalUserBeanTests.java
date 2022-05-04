99c9184b298b026aa321718bb8f382a85f952876
walesyu <wales520@gmail.com>
2022-05-04 13:21:47 +0800

    more test

--- src/test/java/com/uuu/demo1/NormalUserBeanTests.java
+++ src/test/java/com/uuu/demo1/NormalUserBeanTests.java
@@ -1,6 +1,8 @@
 package com.uuu.demo1;
 
 import com.uuu.demo1.beans.NormalUser;
+import com.uuu.demo1.beans.NormalUser2;
+import com.uuu.demo1.beans.NormalUser3;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -28,9 +30,12 @@ public class NormalUserBeanTests {
 
     @Test
     public void test2(){
-        NormalUser normalUser = new NormalUser("");
-        Set<ConstraintViolation<NormalUser>> violations = validator.validate(normalUser);
-        assertThat(violations.size()).isEqualTo(0);
+        NormalUser2 normalUser = new NormalUser2("");
+        Set<ConstraintViolation<NormalUser2>> violations = validator.validate(normalUser);
+        for(ConstraintViolation<NormalUser2> violation: violations){
+            System.out.println(violation.getMessage());
+        }
+        assertThat(violations.size()).isEqualTo(1);
     }
 
     @Test
@@ -38,6 +43,46 @@ public class NormalUserBeanTests {
         NormalUser normalUser = new NormalUser();
         Set<ConstraintViolation<NormalUser>> violations = validator.validate(normalUser);
         for(ConstraintViolation<NormalUser> violation: violations){
+            System.out.println(violation.getMessage());
+        }
+        assertThat(violations.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void test4(){
+        NormalUser2 normalUser = new NormalUser2("          ");
+        Set<ConstraintViolation<NormalUser2>> violations = validator.validate(normalUser);
+        for(ConstraintViolation<NormalUser2> violation: violations){
+            System.out.println(violation.getMessage());
+        }
+        assertThat(violations.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void test5(){
+        NormalUser3 normalUser = new NormalUser3("          ");
+        Set<ConstraintViolation<NormalUser3>> violations = validator.validate(normalUser);
+        for(ConstraintViolation<NormalUser3> violation: violations){
+            System.out.println(violation.getMessage());
+        }
+        assertThat(violations.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void test6(){
+        NormalUser3 normalUser = new NormalUser3();
+        Set<ConstraintViolation<NormalUser3>> violations = validator.validate(normalUser);
+        for(ConstraintViolation<NormalUser3> violation: violations){
+            System.out.println(violation.getMessage());
+        }
+        assertThat(violations.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void test7(){
+        NormalUser3 normalUser = new NormalUser3("");
+        Set<ConstraintViolation<NormalUser3>> violations = validator.validate(normalUser);
+        for(ConstraintViolation<NormalUser3> violation: violations){
             System.out.println(violation.getMessage());
         }
         assertThat(violations.size()).isEqualTo(1);
