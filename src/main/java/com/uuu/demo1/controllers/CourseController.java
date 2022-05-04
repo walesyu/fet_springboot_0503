45e0301167c57fbf5af138d2bb51ea51cfe795c4
Wilson Yu <wilsonyu@fareastone.com.tw>
2022-05-04 14:59:08 +0800

    set form data

--- src/main/java/com/uuu/demo1/controllers/CourseController.java
+++ src/main/java/com/uuu/demo1/controllers/CourseController.java
@@ -1,18 +1,28 @@
 package com.uuu.demo1.controllers;
 
 import com.uuu.demo1.beans.CourseForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class CourseController {
 
+//  private static final Logger LOG = LoggerFactory.getLogger(CourseController.class);
 
   @GetMapping("/course")
   public String getForm(CourseForm form) {
-    form.setCourseId("1234");
-    form.setCourseName("Spring boot");
-    form.setCourseDuration(4);
+//    form.setCourseId("1234");
+//    form.setCourseName("Spring boot");
+//    form.setCourseDuration(4);
     return "courseForm";
+  }
+
+  @PostMapping("/course")
+  public String setForm(CourseForm form) {
+    log.info("course={}", form);
+    return "home";
   }
 }
