0487e92f0ae5dba9e10ad9bd789ff68adccc5a92
Wilson Yu <wilsonyu@fareastone.com.tw>
2022-05-04 14:22:43 +0800

    feat: binding value from controller

--- src/main/java/com/uuu/demo1/beans/CourseForm.java
+++ src/main/java/com/uuu/demo1/beans/CourseForm.java
@@ -1,26 +1,26 @@
 package com.uuu.demo1.beans;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CourseForm {
 
-    @NotEmpty
-    @Size(min = 5, max = 30)
-    private String courseId;
+  @NotEmpty
+  @Size(min = 5, max = 30)
+  private String courseId;
 
-    @NotEmpty
-    private String CourseName;
+  @NotEmpty
+  private String courseName;
 
-    @NotNull
-    @Min(7)
-    private Integer duration;
+  @NotNull
+  @Min(7)
+  private Integer courseDuration;
 }
--- src/main/java/com/uuu/demo1/controllers/CourseController.java
+++ src/main/java/com/uuu/demo1/controllers/CourseController.java
@@ -1,13 +1,18 @@
 package com.uuu.demo1.controllers;
 
+import com.uuu.demo1.beans.CourseForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CourseController {
 
+
   @GetMapping("/course")
-  public String getForm() {
+  public String getForm(CourseForm form) {
+    form.setCourseId("1234");
+    form.setCourseName("Spring boot");
+    form.setCourseDuration(4);
     return "courseForm";
   }
 }
